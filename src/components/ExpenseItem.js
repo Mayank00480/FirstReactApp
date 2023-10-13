ad70a8d2b5d6564872f1d1d31703e0f60af0284c
Mayank00480 <mayanksharma1937@gmail.com>
2023-10-13 22:13:17 +0530

    Added Dynamic Data

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,18 @@
+import './ExpenseItem.css'
 export function ExpenseItem(){
+   const expenseDate = new Date(2021,2,26);
+   const expenseDescription = 'Item Description';
+   const expensePrice = 260;
+   const LocationOfExpenditure = 'Faridabad';
     return(
-   <>
-   <h3>Expense Items</h3>
-   <p>Food Rs 10</p>
-   <p>Petrol Rs 100</p>
-   <p>Movies Rs 200</p>
-   </>
+   <div className='card'>
+   <div className='expense-item'>{expenseDate.toISOString()}</div>
+   <div className = "expense-item__description">
+    <h2 >{expenseDescription}</h2>
+    <div>{LocationOfExpenditure}</div>
+    <div className='expense-item__price'>${expensePrice}</div>
+    
+   </div>
+   </div>
     );
 }
