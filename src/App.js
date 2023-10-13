7420cfb3d60750a773a08db7dbfd779650c0a968
Mayank00480 <mayanksharma1937@gmail.com>
2023-10-14 00:56:06 +0530

    Adding Prop with map function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import { ExpenseItem } from "./components/ExpenseItem";
 
 function App() {
+  const data = [
+    {
+      id : 0,
+     expense : "food",
+     cost : "rs 10",
+     date : "2020-12-11",
+     location : "restorant"
+    },
+    {
+      id:1,
+        expense : "petrol",
+        cost : "rs 100",
+        date : "2020-11-2",
+        location : "bharat petrolium"
+       },
+       {
+        id : 2,
+        expense : "Movies",
+        cost : "rs 200",
+        date : "2020-12-3",
+        location : "rock plaza"
+       },
+  ]
+  
   return (
     <div className="App">
-
-      <ExpenseItem/>
+    {
+      data.map(item => {
+        return <ExpenseItem key = {item.id} title = {item.expense} price = {item.cost} location = {item.location} date = {item.date} />
+      })
+    }
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,18 @@
 import './ExpenseItem.css'
-export function ExpenseItem(){
-   const expenseDate = new Date(2021,2,26);
-   const expenseDescription = 'Item Description';
-   const expensePrice = 260;
-   const LocationOfExpenditure = 'Faridabad';
+export function ExpenseItem(props){
+ 
     return(
-   <div className='card'>
-   <div className='expense-item'>{expenseDate.toISOString()}</div>
-   <div className = "expense-item__description">
-    <h2 >{expenseDescription}</h2>
-    <div>{LocationOfExpenditure}</div>
-    <div className='expense-item__price'>${expensePrice}</div>
-    
-   </div>
-   </div>
+         
+            <div className='card'>
+            <div className='expense-item'>{props.date}</div>
+            <div className = "expense-item__description">
+             <h2 >{props.title}</h2>
+             <div>{props.location}</div>
+             <div className='expense-item__price'>{props.price}</div>
+             
+            </div>
+            </div>
+     
+   
     );
 }
