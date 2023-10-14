fa75fbae235d98fb640828cd327a0bb7a816b207
Mayank00480 <mayanksharma1937@gmail.com>
2023-10-14 13:49:08 +0530

    Delete Expense via DOM Manipulation

--- src/components/Expense/ExpenseItem.js
+++ src/components/Expense/ExpenseItem.js
@@ -3,15 +3,19 @@ import './ExpenseItem.css'
 import ExpenseDetails from './ExpenseDetails';
 import Card from '../UI/Card';
 export function ExpenseItem(props){
- 
+  
+    function deleteExpense(e){
+        console.log("Clicked");
+        e.target.parentElement.remove();
+    }
    
     return(
          
             
-            <Card className='expense-item'>
+            <Card className='expense-item' >
                <ExpenseDate date = {props.date} />
                <ExpenseDetails title = {props.title} location = {props.location} price = {props.price} />
-              
+               <button onClick = {deleteExpense}>Delete</button>
             </Card>
             
      
