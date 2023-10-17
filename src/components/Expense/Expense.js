92deaf34c9910a0d5a98e303dc5856999777b485
Mayank00480 <mayanksharma1937@gmail.com>
2023-10-17 10:58:52 +0530

    Conditional Rendering

--- src/components/Expense/Expense.js
+++ src/components/Expense/Expense.js
@@ -24,7 +24,14 @@ function Expense(props){
   return (
         <Card className = 'expenses'>
           <ExpenseFilter onExpenseFilter = {expenseFilterHandler}/>   
+        {props.data.length == 0 && <p>No Expense to Show</p>}
+          {props.data.length == 1 && (<><ExpenseItem key = {props.data[0].id} 
+              title = {props.data[0].expense} price = {props.data[0].cost} 
+              location = {props.data[0].location} date = {props.data[0].date} />
+            <p>Only one Expense Here Please add more</p> </>)
+            }
           { 
+          props.data.length > 1 && 
             Array.from(props.data).map(item => {
              return <ExpenseItem key = {item.id} 
               title = {item.expense} price = {item.cost} location = {item.location} date = {item.date} />
